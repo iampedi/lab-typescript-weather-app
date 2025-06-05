b7faa44174c3fda45c52269d222bd609f4a03ad4
Pedram Ghane <p.ghane@gmail.com>
2025-06-05 12:31:53 +0200

    Completed lab

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,43 @@
 // src/main.ts
+
+import {
+  getLocation,
+  getCurrentWeather,
+  displayLocation,
+  displayWeather,
+  updateBackground,
+} from "./utils.ts";
+
+const form = document.getElementById("weather-form") as HTMLFormElement;
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const locationInput = document.getElementById("location") as HTMLInputElement;
+  const locationName = locationInput.value;
+  locationInput.value = "";
+
+  getLocation(locationName)
+    .then((response) => {
+      if (response.results) {
+        const location = response.results[0];
+
+        displayLocation(location);
+
+        return getCurrentWeather(location);
+      } else {
+        throw new Error("Location not found");
+      }
+    })
+    .then((weatherData) => {
+      displayWeather(weatherData);
+      updateBackground(
+        weatherData.current_weather.weathercode,
+        weatherData.current_weather.is_day
+      ); // <== ADD THIS
+    })
+    .catch((error) => {
+      console.log("Error getting weather data");
+      console.log(error);
+    });
+});
